2ad626356102cc78c2750308837db5c83fccf084
devesh <19bcs2085@gmail.com>
2021-11-08 13:30:48 +0530

    3rd commit

--- shopify/accounts/views.py
+++ shopify/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render (request,'login.html')
+    
